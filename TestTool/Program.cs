d9a69d9d99228cfc170deeaff5875448e7216dee
DRQSuperior <david.eggins0330@gmail.com>
2022-04-11 17:07:59 -0700

    LOL LIL UPDATE
    LOL LIL UPDATE

--- TestTool/Program.cs
+++ TestTool/Program.cs
@@ -20,7 +20,7 @@ namespace TestTool
             Console.Title = "TestTool";
             void Log(string message)
             {
-                Console.WriteLine("[{0}] {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), message, Color.Orange);
+                Console.WriteLine("[{0}] {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), message, Color.SpringGreen);
             }
 
             void StartCommandHandler()
@@ -51,6 +51,18 @@ namespace TestTool
                             Log("Ping failed: " + ex.Message);
                         }
                     }
+                    else if (command == "flood")
+                    {
+                        FloodClients();
+                    }
+                    else if (command == "clients")
+                    {
+                        GetClients();
+                    }
+                    else if (command == "clear")
+                    {
+                        Console.Clear();
+                    }
                     else
                     {
                         Log("Unknown command");
@@ -66,12 +78,46 @@ namespace TestTool
                 Log("Server started.");
                 while (true)
                 {
-                    //accept a connection
-                    var client = server.AcceptTcpClient();
-                    Log("Client connected.");
-                    StartCommandHandler();
+                    StartClient();
                 }
             }
+
+            void FloodClients()
+            {
+                Log("Creating clients...");
+                var clients = new List<TcpClient>();
+                while (true)
+                {
+                    var client = new TcpClient();
+                    clients.Add(client);
+                    Log("Client connected");
+                }
+            }
+
+            void CreateClient()
+            {
+                var clients = new List<TcpClient>();
+                var client = new TcpClient();
+                clients.Add(client);
+            }
+
+            void GetClients()
+            {
+                Log("Getting clients...");
+                Log("Clients: 0");
+            }
+
+            void StartClient()
+            {
+                Log("Starting client...");
+                var client = new TcpClient();
+                client.Connect(IPAddress.Loopback, 80);
+                CreateClient();
+                Log("Client connected.");
+                StartCommandHandler();
+
+            }
+
             StartServer();
         }
     }
